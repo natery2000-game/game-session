ae4989deec00aa20220ab7bed2ca9f36db9461ae
Nathan <you@example.com>
2021-09-20 08:17:42 -0500

    Adding reading and replying

--- main.go
+++ main.go
@@ -1,18 +1,20 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
+	"strings"
 )
 
 func main() {
 	listenPort := "0.0.0.0:4000"
-	listener, err := net.Listen("tcp4", listenPort)
+	listener, err := net.Listen("tcp", listenPort)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	fmt.Printf("Listening on %s", listenPort)
+	fmt.Printf("Listening on %s\n", listenPort)
 	defer listener.Close()
 
 	for {
@@ -28,4 +30,21 @@ func main() {
 
 func handleConnection(connection net.Conn) {
 	fmt.Printf("Serving %s\n", connection.RemoteAddr().String())
+
+	for {
+		netData, err := bufio.NewReader(connection).ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		message := strings.TrimSpace(string(netData))
+		fmt.Println(message)
+		if message == "STOP" {
+			return
+		}
+
+		result := "Thanks!\n"
+		connection.Write([]byte(string(result)))
+	}
 }
